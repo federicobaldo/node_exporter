340fa83bb0a974f77cb48ae3ec7f1a2e6a76555f
Federico Baldo <federico.baldo@eurotech.com>
2015-09-06 18:19:56 +0100

    Fixed some typos

--- collector/sockstat_linux.go
+++ collector/sockstat_linux.go
@@ -3,7 +3,6 @@
 package collector
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/log"
@@ -12,7 +11,6 @@ import (
 	"os"
 	"regexp"
 	"strconv"
-	"strings"
 )
 
 const (
@@ -46,7 +44,7 @@ func (c *sockStatCollector) Update(ch chan<- prometheus.Metric) (err error) {
 				Namespace: Namespace,
 				Subsystem: sockStatSubsystem,
 				Name:      k,
-				Help:      k + " from /proc/socketstat.",
+				Help:      k + " from /proc/net/sockstat.",
 			})
 		}
 		c.metrics[k].Set(v)
@@ -68,28 +66,31 @@ func getSockStat() (map[string]float64, error) {
 func parseSockStat(r io.Reader) (map[string]float64, error) {
 	var (
 		sockStat = map[string]float64{}
-		scanner  = bufio.NewScanner(r)
-
-		re = regexp.MustCompile("sockets: used \\d+\n" +
+		re       = regexp.MustCompile("sockets: used \\d+\n" +
 			"TCP: inuse (?P<tcp_inuse>\\d+) orphan (?P<orphans>\\d+)" +
 			" tw (?P<tw_count>\\d+) alloc (?P<tcp_sockets>\\d+)" +
 			" mem (?P<tcp_pages>\\d+)\n" +
 			"UDP: inuse (?P<udp_inuse>\\d+)" +
-			//UDP memory accounting was added in v2.6.25-rc1
+			//UDP mem added in v2.6.25-rc1
 			"(?: mem (?P<udp_pages>\\d+))?\n" +
-			//UDP-Lite (RFC 3828) was added in v2.6.20-rc2
+			//UDP-Lite added in v2.6.20-rc2
 			"(?:UDPLITE: inuse (?P<udplite_inuse>\\d+)\n)?" +
 			"RAW: inuse (?P<raw_inuse>\\d+)\n" +
 			"FRAG: inuse (?P<ip_frag_nqueues>\\d+)" +
 			" memory (?P<ip_frag_mem>\\d+)\n")
 	)
 	names := re.SubexpNames()
-	bytesArray, err := ReadAll(scanner)
+	bytesArray, err := ioutil.ReadAll(r)
 
 	match := re.FindSubmatch(bytesArray)
-
 	for k, v := range match {
-		sockStat[names[k]] = float64(v)
+		if k != 0 {
+			log.Debugf("Name: %v, Value: %v", names[k], string(v))
+			sockStat[names[k]], err = strconv.ParseFloat(string(v), 64)
+			if err != nil {
+				break
+			}
+		}
 	}
 
 	return sockStat, err
--- collector/sockstat_linux_test.go
+++ collector/sockstat_linux_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestSockStat(t *testing.T) {
-	file, err := os.Open("fixtures/sockstat")
+	file, err := os.Open("fixtures/sockstats")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -17,11 +17,14 @@ func TestSockStat(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if want, got := 5.0, memInfo["tcp_inuse"]; want != got {
+	t.Logf("%v", sockStat)
+
+	if want, got := 5.0, sockStat["tcp_inuse"]; want != got {
 		t.Errorf("want tcp_inuse %f, got %f", want, got)
 	}
 
-	if want, got := 8.0, memInfo["udp_inuse"]; want != got {
+	if want, got := 8.0, sockStat["udp_inuse"]; want != got {
 		t.Errorf("want udp_inuse %f, got %f", want, got)
 	}
+
 }
